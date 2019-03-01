ffd696f550cbda3c896ca34933f5686c32af1f0e
tytyne <dusaflora2@gmail.com>
2019-03-01 18:02:03 +0200

    correcting identation error

--- app/models.py
+++ app/models.py
@@ -53,12 +53,27 @@ class Pitch(db.Model):
     
         id = db.Column(db.Integer, primary_key=True)
         pitch = db.Column(db.String(255))
+        category = db.Column(db.String(), nullable=False)
+        posted = db.Column(db.DateTime, default=datetime.utcnow)
         user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
         comment = db.relationship('Comment',backref =  'pitch_id',lazy = "dynamic")
 
         def save_pitch(self):
             db.session.add(self)
             db.session.commit()
+
+
+        @classmethod
+        def get_pitches(cls, category):
+            pitches = Pitch.query.filter_by(category=category).all()
+            return pitches
+
+        @classmethod
+        def get_pitch(cls, id):
+            pitch = Pitch.query.filter_by(id=id).first()
+            return pitch
+
+
         
 class Comment(db.Model):
     __tablename__ = 'comment'
